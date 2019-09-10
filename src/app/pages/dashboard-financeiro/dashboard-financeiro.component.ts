5c912258b1788b42efb58cb240c51f141f5f8c18
HectorGuerrini <hectorumaga@hotmail.com>
2019-09-10 11:04:42 -0300

    lista de despesas

--- src/app/pages/dashboard-financeiro/dashboard-financeiro.component.ts
+++ src/app/pages/dashboard-financeiro/dashboard-financeiro.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CalendarEvent, DAYS_OF_WEEK } from 'angular-calendar';
 import * as moment from 'moment';
+import { FinanceiroService } from '../financeiro/financeiro.service';
+import { Tabela, modelTabela } from 'src/app/shared/models/tabela';
 
 @Component({
 	selector: 'app-dashboard-financeiro',
@@ -13,9 +15,32 @@ export class DashboardFinanceiroComponent implements OnInit {
 	viewDate: Date = new Date();
 
 	events: CalendarEvent[] = [];
-	constructor() { }
+	tabActive: Tabela;
+	tabela: Array<modelTabela>;
+	constructor(
+		private financeiroService: FinanceiroService
+	) { }
 
 	ngOnInit() {
+		this.tabActive = new Tabela('despesas',7);
+		this.tabActive.addCol('s');
+		this.tabActive.addCol('s');
+		this.tabActive.addCol('d', moment(),moment());
+		this.tabActive.addCol('d', moment(),moment());
+		this.tabActive.addCol('$');
+		this.tabActive.addCol('s');
+
+		this.getListas();
 	}
 
+	getListas(): void {
+		this.financeiroService.getListaDespesas()
+			.subscribe((data: { query: string; json: Array<modelTabela> }) => {
+				if (data.json.length > 0 ) {
+					this.tabela = data.json;
+				} else {
+					this.tabela = [];
+				}
+			});
+	}
 }
