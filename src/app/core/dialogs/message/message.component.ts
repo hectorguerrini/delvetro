152f13580c866caac20420257cbf3ab4d744a9ed
HectorGuerrini <hectorumaga@hotmail.com>
2019-09-15 20:03:02 -0300

    atualização path

--- src/app/core/dialogs/message/message.component.ts
+++ src/app/core/dialogs/message/message.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { Dialogdata } from '../../shared/models/dialogdata';
+import { Dialogdata } from 'src/app/shared/models/dialogdata';
 @Component({
 	selector: 'app-message',
 	templateUrl: './message.component.html',
