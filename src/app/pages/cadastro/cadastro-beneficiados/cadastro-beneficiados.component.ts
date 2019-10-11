15f3718a4a67cc6a68de80357eea8dbb66929b46
HectorGuerrini <hectorumaga@hotmail.com>
2019-10-11 15:04:18 -0300

    tela beneficiados

--- src/app/pages/cadastro/cadastro-beneficiados/cadastro-beneficiados.component.ts
+++ src/app/pages/cadastro/cadastro-beneficiados/cadastro-beneficiados.component.ts
@@ -1,15 +1,90 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Beneficiados } from '../../../shared/models/beneficiados';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AppService } from 'src/app/core/services/app.service';
+import { distinctUntilChanged } from 'rxjs/operators';
 @Component({
-  selector: 'app-cadastro-beneficiados',
-  templateUrl: './cadastro-beneficiados.component.html',
-  styleUrls: ['./cadastro-beneficiados.component.scss']
+	selector: 'app-cadastro-beneficiados',
+	templateUrl: './cadastro-beneficiados.component.html',
+	styleUrls: ['./cadastro-beneficiados.component.scss']
 })
 export class CadastroBeneficiadosComponent implements OnInit {
+	submitted: boolean = false;
+	beneficiadoForm: FormGroup = this.fb.group({
+		ID_BENEFICIADO: [null],
+		NM_BENEFICIADO: ['', Validators.required],
+		TIPO_BENEFICIADO: ['', Validators.required],
+		CPF: ['', Validators.required],
+		CNPJ: ['', Validators.required],
+		RAZAO_SOCIAL: ['', Validators.required],   				
+		RG: ['', Validators.required],
+		CARGO: ['', Validators.required],
+		SUPERVISOR: ['', Validators.required],
+		USUARIO: ['', Validators.required],
+		SENHA: ['', Validators.required],		
+		SALARIO: ['', Validators.required],
+		DT_CONTRATACAO: ['']		
+	})
 
-  constructor() { }
+	constructor(
+		private fb: FormBuilder,
+		private appService: AppService
+	) { }
+	
+	ngOnInit() {
+		this.onChange();
+	}
 
-  ngOnInit() {
-  }
+	onChange(): void {
+		this.beneficiadoForm.get('TIPO_BENEFICIADO')
+			.valueChanges
+			.pipe(distinctUntilChanged())
+			.subscribe(tipo => {
+				if (tipo === 'Funcionario'){
+					this.beneficiadoForm.get('RG').setValidators([Validators.required]);
+					this.beneficiadoForm.get('SUPERVISOR').setValidators([Validators.required]);
+					this.beneficiadoForm.get('CARGO').setValidators([Validators.required]);
+					this.beneficiadoForm.get('USUARIO').setValidators([Validators.required]);
+					this.beneficiadoForm.get('SENHA').setValidators([Validators.required]);
+					this.beneficiadoForm.get('SALARIO').setValidators([Validators.required]);
 
+					this.beneficiadoForm.get('RG').updateValueAndValidity();
+					this.beneficiadoForm.get('SUPERVISOR').updateValueAndValidity();
+					this.beneficiadoForm.get('CARGO').updateValueAndValidity();
+					this.beneficiadoForm.get('USUARIO').updateValueAndValidity();
+					this.beneficiadoForm.get('SENHA').updateValueAndValidity();
+					this.beneficiadoForm.get('SALARIO').updateValueAndValidity();
+				} else {
+					this.beneficiadoForm.get('RG').setValidators(null);
+					this.beneficiadoForm.get('SUPERVISOR').setValidators(null);
+					this.beneficiadoForm.get('CARGO').setValidators(null);
+					this.beneficiadoForm.get('USUARIO').setValidators(null);
+					this.beneficiadoForm.get('SENHA').setValidators(null);
+					this.beneficiadoForm.get('SALARIO').setValidators(null);
+
+					this.beneficiadoForm.get('RG').updateValueAndValidity();
+					this.beneficiadoForm.get('SUPERVISOR').updateValueAndValidity();
+					this.beneficiadoForm.get('CARGO').updateValueAndValidity();
+					this.beneficiadoForm.get('USUARIO').updateValueAndValidity();
+					this.beneficiadoForm.get('SENHA').updateValueAndValidity();
+					this.beneficiadoForm.get('SALARIO').updateValueAndValidity();
+				}
+
+			});
+
+
+	}
+	salvarBeneficiado(): void {
+		this.submitted = true;
+
+		if (this.beneficiadoForm.invalid){
+			return;
+		}
+		let beneficiado: Beneficiados;
+		beneficiado = this.beneficiadoForm.value;		
+		console.log(beneficiado)
+	}
+	resetForm(): void {
+		this.beneficiadoForm.reset();
+	}
 }
---
+++ src/app/shared/models/beneficiados.ts
@@ -0,0 +1,23 @@
+export class Funcionarios {
+    ID_FUNCIONARIO: number;	
+	NM_FUNCIONARIO: string;
+	RG: string;
+	CARGO: string;
+	SUPERVISOR: string;
+	USUARIO: string;
+	SENHA: string;
+	CPF: string;
+	SALARIO: number;
+	DT_CONTRATACAO: string;
+	SALDO_FERIAS: number;
+	DT_VENCIMENTO_FERIAS: string;
+}
+
+export class Beneficiados extends Funcionarios{
+    ID_BENEFICIADO: number;
+	NM_BENEFICIADO: string;
+	TIPO_BENEFICIADO: string;
+	CPF: string;
+	CNPJ: string;
+    RAZAO_SOCIAL: string;   
+}
